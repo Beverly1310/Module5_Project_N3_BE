eaec3adaf6a279f15d0a5b0d50e8dad630a98759
TrongKD <khuatdinhtrong@gmail.com>
2024-07-22 21:25:50 +0700

    task done

--- src/main/java/com/ra/controller/AdminController.java
+++ src/main/java/com/ra/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.ra.controller;
 
+import com.ra.model.dto.req.BannerAdd;
+import com.ra.model.dto.req.BannerEdit;
 import com.ra.model.dto.req.CategoryForm;
 import com.ra.model.dto.req.ProductForm;
 import com.ra.model.dto.res.ProductResponse;
+import com.ra.model.entity.Banner;
 import com.ra.model.entity.Category;
 import com.ra.service.ICategoryService;
 import com.ra.service.IProductService;
@@ -12,13 +15,13 @@ import org.springframework.data.domain.Page;
 import com.ra.model.entity.User;
 import com.ra.service.AdminService;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -118,24 +121,49 @@ public class AdminController {
         productService.delete(id);
         return ResponseEntity.ok().build();
     }
-        @GetMapping("/user")
-        public ResponseEntity<Page<User>> getAllUsers
-        (@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
-                @RequestParam(required = false) String direction,
-                @RequestParam(defaultValue = "") String search
-                                       ){
-            Page<User> users = adminService.getUserWithPagingAndSorting(pageable, direction, search);
-            return ResponseEntity.ok().body(users);
-        }
-        @PutMapping("/user/{userId}")
-        public ResponseEntity<User> changeStatus (@PathVariable("userId") Long userId){
-            User user = adminService.changStatus(userId);
-            return ResponseEntity.ok().body(user);
-        }
-        @PutMapping("user/setrole/{userId}")
-        public ResponseEntity<User> changeRole (@PathVariable("userId") Long userId){
-            User user = adminService.setRole(userId);
-            return ResponseEntity.ok().body(user);
-        }
+
+    @GetMapping("/user")
+    public ResponseEntity<Page<User>> getAllUsers
+            (@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+             @RequestParam(required = false) String direction,
+             @RequestParam(defaultValue = "") String search
+            ) {
+        Page<User> users = adminService.getUserWithPagingAndSorting(pageable, direction, search);
+        return ResponseEntity.ok().body(users);
     }
+
+    @PutMapping("/user/{userId}")
+    public ResponseEntity<User> changeStatus(@PathVariable("userId") Long userId) {
+        User user = adminService.changStatus(userId);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PutMapping("user/setrole/{userId}")
+    public ResponseEntity<User> changeRole(@PathVariable("userId") Long userId) {
+        User user = adminService.setRole(userId);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @GetMapping("/banner")
+    public ResponseEntity<List<Banner>> getAllBanners() {
+        List<Banner> banners = adminService.getBanners();
+        return ResponseEntity.ok().body(banners);
+    }
+
+    @PostMapping("/banner")
+    public ResponseEntity<Banner> addBanner(@Valid @ModelAttribute BannerAdd bannerAdd) {
+        Banner banner = adminService.addBanner(bannerAdd);
+        return ResponseEntity.ok().body(banner);
+    }
+    @DeleteMapping("/banner/{bannerId}")
+    public ResponseEntity<?> deleteBanner(@PathVariable  Long bannerId) {
+        adminService.deleteBanner(bannerId);
+        return ResponseEntity.ok().body("Deleted banner");
+    }
+    @PutMapping("/banner")
+    public ResponseEntity<?> updateBanner(@Valid @ModelAttribute BannerEdit bannerEdit) {
+        Banner banner = adminService.updateBanner(bannerEdit);
+        return ResponseEntity.ok().body(banner);
+    }
+}
 
