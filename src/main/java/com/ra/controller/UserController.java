f180f2ed0d4e50a1a8104ca54310430b122e2331
TheFrozenWatcher <hungnguyen1998ylvp@gmail.com>
2024-07-25 17:01:40 +0700

    Your commit message describing the changes

--- src/main/java/com/ra/controller/UserController.java
+++ src/main/java/com/ra/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ra.controller;
 
+import com.ra.model.dto.req.CartItemRequest;
 import com.ra.model.dto.req.ChangePasswordRequest;
 import com.ra.model.dto.req.CommentRequest;
 import com.ra.model.dto.req.UserEdit;
@@ -32,6 +33,7 @@ public class UserController {
     private final UserService userService;
     private final IWishListService wishListService;
     private final ICommentService commentService;
+    private final ICartService cartService;
 
     //    Display categories and products
     @GetMapping("/category/{id}")
@@ -55,6 +57,12 @@ public class UserController {
         ProductResponse productResponse = productService.findById(id);
         productResponse.setOnWishlist(wishListService.isFavourite(id));
         return ResponseEntity.ok(productResponse);
+    }
+
+//    Get related products
+    @GetMapping("product/{id}/related")
+    public ResponseEntity<?> getRelatedProducts(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getRelatedProducts(id));
     }
 
     //    View comments
@@ -111,13 +119,49 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    //    Shopping cart
+    @GetMapping("/cart")
+    public ResponseEntity<?> getAllCartItems() {
+        CartListResponse cartListResponse=cartService.getAllItemsInCart();
+        return ResponseEntity.ok(cartListResponse);
+    }
 
+    @GetMapping("/getCartCount")
+    public ResponseEntity<?> getCartCount() {
+        return ResponseEntity.ok(cartService.cartNumber());
+    }
+
+    @PostMapping("/cart/add")
+    public ResponseEntity<?> addItemToCart(@Valid @RequestBody CartItemRequest request){
+        cartService.add(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/cart/update")
+    public ResponseEntity<?> changeItemQuantity(@Valid @RequestBody CartItemRequest request){
+        cartService.changeQuantity(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/cart/{cartId}")
+    public ResponseEntity<?> removeItemFromCart(@PathVariable Long cartId){
+        cartService.deleteItemFromCart(cartId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("cart/clearAll")
+    public ResponseEntity<?> clearAllCartItems(){
+        cartService.deleteAllItems();
+        return ResponseEntity.ok().build();
+    }
     //  Wishlist
     @PostMapping("/product/{productId}/wishlist")
     public ResponseEntity<?> addToWishlist(@PathVariable long productId) {
         wishListService.addOrDeleteFromWishList(productId);
         return ResponseEntity.ok().build();
     }
+
+
 
     @PutMapping("/edit")
     public ResponseEntity<ResponseData<User>> editAccount(@Valid @ModelAttribute UserEdit userEdit) {
--- src/main/java/com/ra/repository/OrderDetailRepository.java
+++ src/main/java/com/ra/repository/OrderDetailRepository.java
@@ -1,2 +1,13 @@
-package com.ra.repository;public interface OrderDetailRepository {
+package com.ra.repository;
+
+import com.ra.model.entity.OrderDetail;
+import com.ra.model.entity.ProductDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail,Long> {
+    List<OrderDetail> findAllByIdOrderId(Long orderId);
 }
